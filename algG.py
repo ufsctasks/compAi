258d6f87c4972480b622e780849f8486926cdd15
pitaemir <emirbraz.d2@gmail.com>
2024-09-22 13:03:52 -0300

    algoritmo guloso

---
+++ algG.py
@@ -0,0 +1,17 @@
+def algoritmo_moeda(troco, moedas_disponiveis):
+    resultado = []
+    for moeda in sorted(moedas_disponiveis, reverse=True):
+        while troco >= moeda:
+            troco -= moeda
+            resultado.append(moeda)
+    return resultado
+
+# Exemplo de uso
+moedas_disponiveis = [50, 25, 10, 5, 1]
+
+troco = 28
+
+resultado = algoritmo_moeda(troco, moedas_disponiveis)
+
+print(f"Menor número de moedas: {len(resultado)}")
+print(f"Moedas usadas: {resultado}")
